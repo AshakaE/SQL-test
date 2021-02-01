c256b4945e417fe4032505c07f2c081f9066e328
AshakaE <ashakaegerega@gmail.com>
2021-02-01 10:23:21 +0100

    NULL

--- results.sql
+++ results.sql
@@ -520,3 +520,58 @@ SELECT DISTINCT name
 FROM actor JOIN casting ON id=actorid
 WHERE movieid IN (SELECT movieid
   FROM casting JOIN actor ON (actorid=id AND name='Art Garfunkel')) AND name != 'Art Garfunkel'
+
+-- NULL 
+-- 1
+SELECT name
+FROM teacher
+WHERE dept IS NULL
+
+-- 2
+SELECT teacher.name,
+  dept.name
+FROM teacher
+  INNER JOIN dept ON (teacher.dept = dept.id)
+
+-- 3
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept
+  ON (teacher.dept=dept.id)
+
+-- 4
+SELECT teacher.name, dept.name
+FROM teacher RIGHT JOIN dept
+  ON (teacher.dept=dept.id)
+
+-- 5
+SELECT COALESCE(teacher.name, 'NONE'), COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+
+-- 6
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
+
+-- 7
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
+
+-- 8
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept ON (teacher.dept=dept.id)
+GROUP BY dept.name
+
+-- 9
+SELECT teacher.name,
+  CASE WHEN dept.id = 1 THEN 'Sci'
+     WHEN dept.id = 2 THEN 'Sci'
+     ELSE 'Art' END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+
+-- 10
+SELECT teacher.name,
+  CASE 
+WHEN dept.id = 1 THEN 'Sci'
+WHEN dept.id = 2 THEN 'Sci'
+WHEN dept.id = 3 THEN 'Art'
+ELSE 'None' END
+FROM teacher LEFT JOIN dept ON (dept.id=teacher.dept)
